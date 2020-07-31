a42bed89829fe2f818b343762e82769e2d8951bb
chenlhlinux <chenlhlinux@outlook.com>
2020-07-31 22:21:31 +0800

    分离docs并清洗历史

---
+++ js/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d319f0d6bfeeb585b4d7354b0c8beb8b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
